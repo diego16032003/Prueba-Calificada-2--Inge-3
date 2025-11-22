b32a2004c394dab4d426b23e5369641342fda4f3
Diego <piscis182116@gmail.com>
2025-11-21 22:00:32 -0500

    Commit tarea022

--- src/main/java/org/nsq/modelo/Producto.java
+++ src/main/java/org/nsq/modelo/Producto.java
@@ -1,26 +1,140 @@
 package org.nsq.modelo;
 
-public class Producto {
-    //Atributos
-    private String nombre;
-    private double precio;
-    private int cantidad;
+import java.util.Objects;
 
-    public Producto(String nombre, double precio, int cantidad) {
+public class Producto{
+
+
+    private String nombre;               // Ej: "Laptop"
+    private double precio;               // Ej: 1200.50
+    private int cantidad;
+    private String sku;                  /
+    private String categoria;
+    private boolean esActivo;
+    private boolean descuentoAplicable;
+
+
+    public Producto(String nombre, double precio, int cantidad,
+                    String sku, String categoria,
+                    boolean esActivo, boolean descuentoAplicable) {
+
+        validarPrecio(precio);
+        validarCantidad(cantidad);
+
         this.nombre = nombre;
         this.precio = precio;
         this.cantidad = cantidad;
+        this.sku = sku;
+        this.categoria = categoria;
+        this.esActivo = esActivo;
+        this.descuentoAplicable = descuentoAplicable;
+    }
+
+    public Producto(String sku) {
+        this.sku = sku;
+        this.nombre = "Producto sin nombre";
+        this.precio = 0;
+        this.cantidad = 0;
+        this.categoria = "General";
+        this.esActivo = true;
+        this.descuentoAplicable = false;
+    }
+    -
+    private void validarPrecio(double precio) {
+        if (precio < 0) {
+            throw new IllegalArgumentException("El precio no puede ser negativo.");
+        }
+    }
+
+    private void validarCantidad(int cantidad) {
+        if (cantidad < 0) {
+            throw new IllegalArgumentException("La cantidad no puede ser negativa.");
+        }
     }
 
     public String getNombre() {
         return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public double getPrecio() {
         return precio;
+    }
+
+    public void setPrecio(double precio) {
+        validarPrecio(precio);
+        this.precio = precio;
     }
 
     public int getCantidad() {
         return cantidad;
     }
+
+    public void setCantidad(int cantidad) {
+        validarCantidad(cantidad);
+        this.cantidad = cantidad;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public boolean isEsActivo() {
+        return esActivo;
+    }
+
+    public void setEsActivo(boolean esActivo) {
+        this.esActivo = esActivo;
+    }
+
+    public boolean isDescuentoAplicable() {
+        return descuentoAplicable;
+    }
+
+    public void setDescuentoAplicable(boolean descuentoAplicable) {
+        this.descuentoAplicable = descuentoAplicable;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Producto producto = (Producto) obj;
+        return Objects.equals(sku, producto.sku);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sku);
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", cantidad=" + cantidad +
+                ", sku='" + sku + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", esActivo=" + esActivo +
+                ", descuentoAplicable=" + descuentoAplicable +
+                '}';
+    }
 }
+
